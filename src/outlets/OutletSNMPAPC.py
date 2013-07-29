b49b9b5bc3dae380f448d1e501e41f2a9481ab2f
E.S. Rosenberg <esr+ocklesource@mail.hebrew.edu>
2013-07-29 16:13:41 +0300

    indentation fixes and minor bug in updateData() where the right member of the tupple wasn't being selected

--- src/outlets/OutletSNMPAPC.py
+++ src/outlets/OutletSNMPAPC.py
@@ -74,7 +74,7 @@ class APC_Rack_PDU(OutletTemplate):
                                                      snmpCreateAuthData(
                                                          self.snmp_version,
                                                          self.ReadCommunity),
-                                                     oid))
+                                                     oid)[1])
         except Exception as e:
             self.data["current"] = 0
             print e
@@ -82,21 +82,21 @@ class APC_Rack_PDU(OutletTemplate):
         return self.data
 
     def haveFun(self, interval):
-    '''Function to turn all outlets on and off sequentially
-    '''
-    state = 1
-    import time
-    while 1 == 1:
-        for outlet in range(1,25):
-            try:
-                snmpSetter(self.hostame, self.snmp_port, snmpCreatAuthData(
-                    self.snmp_version, snmp.WriteCommunity), 
-                           ('1.3.6.1.4.1.318.1.1.4.4.2.1.3.{}'.format(outlet), 
-                            rfc1902.Integer(state)))
-            except Exception as e:
-                print e
-                time.sleep(interval)
-                if state == 1:
-                    state = 2
-                elif state == 2:
-                    state = 1
+        '''Function to turn all outlets on and off sequentially
+        '''
+        state = 1
+        import time
+        while 1 == 1:
+            for outlet in range(1,25):
+                try:
+                    snmpSetter(self.hostame, self.snmp_port, snmpCreatAuthData(
+                        self.snmp_version, snmp.WriteCommunity), 
+                    ('1.3.6.1.4.1.318.1.1.4.4.2.1.3.{}'.format(outlet), 
+                     rfc1902.Integer(state)))
+                except Exception as e:
+                    print e
+                    time.sleep(interval)
+                    if state == 1:
+                        state = 2
+                    elif state == 2:
+                        state = 1
